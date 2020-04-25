6a6f8ac39a30cde17a257f4f0bc311519f3e3fd2
Alfian Rahman <46818494+alefianrahman@users.noreply.github.com>
2020-04-25 23:49:06 +0700

    Update words_rotation .py
    Restructure the script to a function and adding some comments

--- words_rotation .py
+++ words_rotation .py
@@ -1,20 +1,30 @@
+"""
+This library will find the possible combinations of words rotation in a given input. 
+"""
+
+# List of input words
 arr_input = ['Tokyo', 'London', 'Rome', 'Donlon', 'Kyoto', 'Paris', 'Okyot']
-output = []
-used_words = []
 
-# generate all possible combinations from arr_input
-for input in arr_input:
-    single_array = []
-    for i in range(len(input)):
-        a = input[i] + input[i+1:] + input[:i]
-        a = a.title()
-        if a in arr_input and a not in used_words:
-            single_array.append(a)
-            used_words.append(a)
-    output.append(single_array)
+# Generate all possible combinations from a list of words
+def words_rotation(list_input=arr_input):
+    """
+    This function will find the possible combinations of words rotation in a given input.
+    """
+    output = []  # initiate empty list for the output
+    used_words = []  # initiate empty list to store the used words
+    for input in list_input:
+        single_array = []  # initiate empty list to store the possible combinations for each word
+        for i in range(len(input)):
+            a = input[i] + input[i+1:] + input[:i]  # Create a word by shifting its position
+            a = a.title()
+            if a in list_input and a not in used_words: # Check if there is any matching word
+                single_array.append(a)
+                used_words.append(a)
+        output.append(single_array)
 
-output = [i for i in output if len(i) > 0]
-print(output)
+    output = [i for i in output if len(i) > 0]
+    print(output)
 
-        
 
+if __name__ == '__main__':
+    words_rotation()
